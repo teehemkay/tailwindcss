33b472efa3939fe12adb8ba6d94f2d7a71826edc
MABONZO Kalimasi Nzau-Mfuka <kalimasi.mabonzo@ext.europarl.europa.eu>
2025-03-07 13:46:22 +0100

    chore: +rust toolchain

--- flake.nix
+++ flake.nix
@@ -5,35 +5,44 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default-darwin";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
 
     # Override flake-utils systems to mine
     flake-utils.inputs.systems.follows = "systems";
   };
 
-  outputs = { self, nixpkgs, systems, flake-utils, ... }:
+  outputs = { self, nixpkgs, systems, flake-utils, rust-overlay, ... }:
     let
       eachSystem = f:
-        flake-utils.lib.eachSystem (import systems)
-        (system: f nixpkgs.legacyPackages.${system});
+        flake-utils.lib.eachSystem (import systems) (system:
+          f (import nixpkgs {
+            inherit system;
+            overlays = [ rust-overlay.overlays.default ];
+          }));
 
     in eachSystem (pkgs:
       let
         inherit (pkgs) stdenv lib mkShell;
         inherit (pkgs.darwin.apple_sdk.frameworks) CoreFoundation CoreServices;
 
+        rustToolchain =
+          pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
       in {
         devShells = {
           default = mkShell {
             packages = [
+              pkgs.astro-language-server
               pkgs.html-tidy
               pkgs.marksman
               pkgs.nodejs
               pkgs.pnpm_9
-              pkgs.typescript
               pkgs.tailwindcss-language-server
+              pkgs.typescript
               pkgs.typescript-language-server
               pkgs.vscode-langservers-extracted
               pkgs.yaml-language-server
+              rustToolchain
             ] ++ lib.optionals stdenv.isDarwin [ CoreFoundation CoreServices ];
           };
         };
